8310848736cbfb5e036f664be51022da08937919
Jaryd Knight <jaryd.knight@hotmail.com>
2022-05-19 22:37:22 -0400

    create index.html and game.js

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+// Fight function
+
+const fight = () => {
+    window.alert("The fight has begun!")
+};
+
+// fight();
+
+const playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
