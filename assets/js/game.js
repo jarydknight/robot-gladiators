95f414d92687c863b140c20d42d0f72cfd017fd2
Jaryd Knight <jaryd.knight@hotmail.com>
2022-05-23 11:59:04 -0400

    random attack and health values

--- assets/js/game.js
+++ assets/js/game.js
@@ -43,7 +43,7 @@ var fight = function(enemyName) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
 
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
 
                 console.log("playerMoney", playerMoney);
 
@@ -51,16 +51,19 @@ var fight = function(enemyName) {
             }
         }
 
-            //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-            enemyHealth = enemyHealth - playerAttack;
+            // generate random damage value based on player's attack power
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+
+            enemyHealth = Math.max(0, enemyHealth - damage);
 
             // Log a resulting message to the console so we know that it worked.
             console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
 
-            /// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+            playerHealth = Math.max(0, playerHealth - damage);
 
             // Log a resulting message to the console so we know that it worked.
             console.log(
@@ -85,6 +88,13 @@ var fight = function(enemyName) {
         
 }
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+  };
+
 const startGame = () => {
 
     playerHealth = 100;
@@ -100,7 +110,7 @@ const startGame = () => {
     
             let pickedEnemyName = enemyNames[i]
             
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
         
             fight(pickedEnemyName);
 
